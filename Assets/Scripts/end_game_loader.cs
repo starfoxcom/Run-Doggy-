3f153cf3b88a71add4af53549caf21966368e244
frankivolt <idv16c.falvarado@uartesdigitales.edu.mx>
2019-01-26 12:28:16 -0600

    Add comments to script and delete update function

--- Assets/Scripts/end_game_loader.cs
+++ Assets/Scripts/end_game_loader.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class end_game_loader : MonoBehaviour
 {
-    public GameObject [] endsprite;
-    public bool win;
+
+    public GameObject [] endsprite; //sprite array
+    public bool win; //win bool state
+
     // Start is called before the first frame update
     void Start()
     {
+        
+        //On win
         if(win)
         {
+
+            //Enable sprite 0
             endsprite[0].GetComponent<SpriteRenderer>().enabled = true;
         }
+
+        //Otherwise
         else
         {
+
+            //Enable sprite 1
             endsprite[1].GetComponent<SpriteRenderer>().enabled = true;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
