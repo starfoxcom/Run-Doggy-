60ee9ad6dd0f2d1895a580053f0ab7e55b3ede4a
frankivolt <idv16c.falvarado@uartesdigitales.edu.mx>
2019-01-26 11:56:58 -0600

    Create scene with victory and game over sprites triggered options

---
+++ Assets/Scripts/end_game_loader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class end_game_loader : MonoBehaviour
+{
+    public GameObject [] endsprite;
+    public bool win;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(win)
+        {
+            endsprite[0].GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else
+        {
+            endsprite[1].GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
