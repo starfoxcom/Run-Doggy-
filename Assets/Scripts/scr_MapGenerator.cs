982b31dc50445e2453ae8db5e0e733280c0624df
nuup20 <idv16a.msolano@uartesdigitales.edu.mx>
2019-01-26 10:46:17 -0600

    MapGenerator

--- Assets/Scripts/scr_MapGenerator.cs
+++ Assets/Scripts/scr_MapGenerator.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scr_MapGenerator : MonoBehaviour
+public class scr_MapGenerator 
+    : MonoBehaviour
 {
+    //////////////////////////////////////////////////////////////////////////
+    // Public Methods                                                       //
+    //////////////////////////////////////////////////////////////////////////
+
+    /// <summary>
+    /// Loads a scene from file
+    /// </summary>
+    /// <param name="_fileName"></param>
+    public void 
+    LoadScene(string _fileName)
+    {
+        // Get File
+        TextAsset file;
+        file = Resources.Load(_fileName) as TextAsset;
+
+
+    }
+
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
+    { }
 
     // Update is called once per frame
     void Update()
-    {
-        
-    }
+    { }
 }
