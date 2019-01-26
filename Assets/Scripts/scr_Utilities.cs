a576ecad5d312df4d8d6527258c91ea61c875542
Mick76 <idv18b.jcerda@uartesdigitales.edu.mx>
2019-01-26 10:48:12 -0600

    CsvReader script added

---
+++ Assets/Scripts/scr_Utilities.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_Utilities : MonoBehaviour
+{
+    enum MyEnum
+    {
+
+    }
+
+    static string GiveTileType(string number)
+    {
+        string tipo = "";
+
+
+
+        return tipo;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
