ebac861e961a8eade05d478124f844e9cce8a2d6
Jorge zamudio <idv15c.jzamudio@uartesdigitales.edu.mx>
2019-01-26 18:42:13 -0600

    Add loading times for the animation to finish and load the scene after

---
+++ Assets/Scripts/load_async.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class load_async : MonoBehaviour
+{
+
+  private AsyncOperation async; //Async operation
+
+  [SerializeField]
+  private int level = 0; //Scene number
+
+  [SerializeField]
+  private float timeLeft = 5; //Time for animation
+
+  //Coroutine to load the main/game scene on loading scene
+  IEnumerator LoadLevelWithBar(int level)
+  {
+
+    //while theres still time left
+    while (timeLeft > 0)
+    {
+
+      yield return null;
+    }
+
+    //Assign async op as the scene to load in an async method
+    async = SceneManager.LoadSceneAsync(level);
+  }
+
+  // Start is called before the first frame update
+  private void Start()
+  {
+
+    //Start the coroutine
+    StartCoroutine(LoadLevelWithBar(level));
+  }
+
+  private void Update()
+  {
+
+    timeLeft -= Time.deltaTime;
+    Debug.Log(timeLeft);
+  }
+}
